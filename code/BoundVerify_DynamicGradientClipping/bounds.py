fix sign of Delta G

code/BoundVerify_DynamicGradientClipping/bounds.py
@@ -7,6 +7,17 @@ from torch.utils.data import DataLoader
 from myhessian import iHVP, HVP, _inner_product
 from utils import ClippedCrossEntropyLoss
 from tqdm.auto import tqdm
+import numpy as np
+def average_hessian_trace(clip, model, dataloader: DataLoader):
+        trace_sum = 0
+        count = 0
+        for X, Y in dataloader:
+            model.zero_grad(True)
+            batch_mean_trace = np.mean(hessian(model, criterion=ClippedCrossEntropyLoss(clip), data=(X, Y), cuda=True).trace())
+            trace_sum = trace_sum + batch_mean_trace * len(Y)
+            count += len(Y)
+            model.zero_grad(True)
+        return trace_sum / count
 
 
 class Bound(nn.Module):
@@ -53,13 +64,14 @@ class GradientDispersionBound(Bound):
 
 
 class TerminalDispersionBound(Bound):
-    def __init__(self, clip=None, flatness=True, cross_dispersion=False, full_utilization=False) -> None:
+    def __init__(self, clip=None, flatness=True, cross_dispersion=False, full_utilization=False, traj_reweight=1.0) -> None:
         super().__init__()
         self._n_iter = nn.Parameter(torch.zeros([1], dtype=torch.int), requires_grad=False)
         self.clip = clip
         self.flatness = flatness
         self.cross_dispersion = cross_dispersion
         self.full_utilization = full_utilization
+        self.traj_reweight = traj_reweight
 
     @property
     def n_iter(self):
@@ -76,6 +88,8 @@ class TerminalDispersionBound(Bound):
                     res = res + "_full_utilization"
             else:
                 res = res + "_possibly_biased"
+            if self.traj_reweight != 1.0:
+                res = res + f"+reweight{self.traj_reweight:.3f}"
         return res
 
     @torch.no_grad()
@@ -124,32 +138,24 @@ class TerminalDispersionBound(Bound):
         grad_empirical = self.gradients(parallel_model, parallel_training_data_loader)
         grad_population = self.gradients(parallel_model, test_dataloader) 
         diff_grad = [[
-            grad_population[index_model][index_param] 
-                - grad_empirical[index_model][index_param] 
+            grad_empirical[index_model][index_param] 
+                - grad_population[index_model][index_param] 
             # torch.zeros_like(grad_empirical[index_model][index_param])
                     for index_param in range(len(grad_empirical[0]))] for index_model in range(len(grad_empirical))] 
         Delta = iHVP(
             parallel_model,
             [[
                 (SelectedDataFieldDataLoader(parallel_training_data_loader.loaders[i], data_field=[0, 1]), 1), 
-                # (selected_testing_loader, -1), 
-                2 * C[i]
+               # (selected_testing_loader, -1), 
+                2 * C[i] * self.traj_reweight 
             ] for i in range(len(parallel_model))],
             [[
-                2 * C[index_model] * nu[index_model][index_param] - diff_grad[index_model][index_param]  
+                - 2 * C[index_model] * self.traj_reweight * nu[index_model][index_param] - diff_grad[index_model][index_param]  
                 for index_param in range(len(nu[0]))] 
             for index_model in range(len(nu))],
-            1e-2,
+            4e-2,
             clip=self.clip
         )
-
-        # punishment1 = torch.stack([torch.stack([torch.inner(a.flatten(), b.flatten()) for a, b in zip(m_g, m_d)]).sum() for m_g, m_d in zip(diff_grad, Delta)]).mean() 
-        # punishment2 = torch.stack([
-                # _inner_product(delta, HVP([
-                    # (hessian(model, criterion=ClippedCrossEntropyLoss(clip=self.clip), dataloader=SelectedDataFieldDataLoader(emprical_loader, data_field=[0, 1]), cuda=True), 1),
-                    # # (hessian(model, criterion=ClippedCrossEntropyLoss(clip=self.clip), dataloader=selected_testing_loader, cuda=True), -1)
-                # ], delta))
-        # for emprical_loader, delta, model in  zip(parallel_training_data_loader.loaders, Delta, parallel_model.models)]).mean()/2
 
         return Delta
 
@@ -166,24 +172,26 @@ class TerminalDispersionBound(Bound):
 
         return torch.stack(losses).mean()
 
+
+
     def gamma(self, delta: 'list[Tensor]', model: nn.Module, loader: DataLoader, trace=True):
         loss0 = self.loss(model, loader)
         torch.cuda.synchronize()
         with torch.no_grad():
             for (d, p) in zip(delta, model.parameters()):
-                p.data -= d
+                p.data += d
         torch.cuda.synchronize()
         loss_delta = self.loss(model, loader)
         if trace:
-            hessian_traces = hessian(model, criterion=ClippedCrossEntropyLoss(self.clip), dataloader=SelectedDataFieldDataLoader(loader, [0, 1]), cuda=True).trace()
+            hessian_traces = average_hessian_trace(self.clip, model, SelectedDataFieldDataLoader(loader, [0, 1]))
             hessian_trace = torch.tensor(hessian_traces).mean().to(device=next(model.parameters()).device)
-            model.zero_grad()
+            model.zero_grad(True)
         else:
             hessian_trace = None
         torch.cuda.synchronize()
         with torch.no_grad():
             for (d, p) in zip(delta, model.parameters()):
-                p.data += d
+                p.data -= d
         torch.cuda.synchronize()
         return loss_delta - loss0, hessian_trace
 
@@ -195,8 +203,9 @@ class TerminalDispersionBound(Bound):
         delta_losses = []
         for delta, model, empirical_loader in zip(tqdm(Delta, "punishing"), parallel_model.models, parallel_training_data_loader.loaders):
             empirical_delta_loss, hessian_trace = self.gamma(delta, model, empirical_loader)
-            population_delta_loss, _ = self.gamma(delta, model, test_data_loader, False)
-            hessian_traces.append(hessian_trace)
+            population_delta_loss,  population_hessian_trace = self.gamma(delta, model, test_data_loader, True)
+            # population_hessian_trace = 0
+            hessian_traces.append((hessian_trace - population_hessian_trace).abs())
             delta_losses.append(empirical_delta_loss - population_delta_loss)
         return torch.stack(delta_losses).mean(),  torch.stack(hessian_traces).mean()
 
@@ -244,6 +253,7 @@ class TerminalDispersionBound(Bound):
             return self.trajectory_term(parallel_model), 0, None, {}
         nu = self.get_nu(parallel_model)
         Delta = self.surrogate_forget(C, nu, parallel_model, parallel_training_data_loader, test_data_loader)
+        # Delta = nu
 
         with torch.no_grad():
             tensor_delta = torch.stack([torch.cat([p.flatten() for p in m]) for m in Delta], dim=0)

code/BoundVerify_DynamicGradientClipping/main.py
@@ -5,10 +5,11 @@ import numpy as np
 import torch
 from torch import Tensor
 import torch.nn as nn
+import torch.utils
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, Subset
 from torch.distributions import Normal
 from pyhessian import hessian  # Hessian computation
 from utils import *
@@ -18,8 +19,8 @@ import time
 import random
 from tqdm.auto import tqdm
 from collections import OrderedDict
-from parallel import ParallelModel, ParallelLoss, make_parallel_datasets, ParallelDataloader
-from bounds import GradientDispersionBound, TerminalDispersionBound, Bound
+from parallel import ParallelModel, ParallelLoss, make_parallel_datasets, ParallelDataloader, SelectedDataFieldDataLoader
+from bounds import GradientDispersionBound, TerminalDispersionBound, Bound, average_hessian_trace
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -29,7 +30,12 @@ torch.autograd.set_detect_anomaly(True)
 def accuracy(output: Tensor, targets: Tensor):
     return (output.argmax(dim=-1) == targets).float().mean()
 
-    
+def subset(datasets, k):
+    if isinstance(datasets, list):
+        return [subset(d, k) for d in datasets]
+    indices = torch.randperm(len(datasets))[:int(len(datasets) * k)]
+    return Subset(datasets, indices) 
+
 
 class RunModel(nn.Module):
     def __init__(self, args):
@@ -58,8 +64,9 @@ class RunModel(nn.Module):
             GradientDispersionBound(), 
             TerminalDispersionBound(clip=self.clip, flatness=False), 
             # TerminalDispersionBound(clip=self.loss_clip),
-            # TerminalDispersionBound(clip=self.loss_clip, cross_dispersion=True),
             TerminalDispersionBound(clip=self.loss_clip, cross_dispersion=True, full_utilization=True),
+            # TerminalDispersionBound(clip=self.loss_clip, cross_dispersion=True),
+            TerminalDispersionBound(clip=self.loss_clip, cross_dispersion=True, full_utilization=True, traj_reweight=args.traj_reweight),
             # TerminalDispersionBound(clip=self.loss_clip, unbiased=True, trajectories_for_opt=args.trajectories_for_optimization)
         ])
 
@@ -77,8 +84,8 @@ class RunModel(nn.Module):
         testdataset = InMemoryDataset(MyDataset(args, _train=False))
 
         train_loader = ParallelDataloader(self.traindataset, batch_size=args.batch_size, shuffle=shuffle_train, num_workers=4, pin_memory=True, persistent_workers=True)
-        train_test_loader = ParallelDataloader(train_test_dataset, batch_size=512, num_workers=4, pin_memory=True, persistent_workers=True)
-        test_loader = DataLoader(testdataset, batch_size=512, shuffle=False, num_workers=4, pin_memory=True, persistent_workers=True)
+        train_test_loader = ParallelDataloader(subset(train_test_dataset, args.data_usage_for_bounds), batch_size=256, num_workers=4, pin_memory=True, persistent_workers=True)
+        test_loader = DataLoader(subset(testdataset, args.data_usage_for_bounds), batch_size=256, shuffle=False, num_workers=4, pin_memory=True, persistent_workers=True)
         if args.dataset == "mnist":
             from archs.mnist import AlexNet, LeNet5, fc1, vgg, resnet
         if args.dataset == "cifar10":
@@ -167,26 +174,16 @@ class RunModel(nn.Module):
         return tr_losses, tr_acces, ts_losses, ts_acces
 
     def train_epoch(self, args, train_loader):
-        # """Train for one epoch on the training set"""
-        # losses = AverageMeter()
-        # norm_mean = AverageMeter()
-        # variance_mean = AverageMeter()
-        # batch_mean = AverageMeter()
         self.model.train()
         for batch_idx, data in enumerate(tqdm(train_loader)):
             inputs, labels, idx = data
-            # loss, norm, variance, batch_norm = 
             self.train_batch(inputs, labels, idx, args)
-            # losses.update(loss.item(), inputs.size(0))
-            # norm_mean.update(norm, inputs.size(0))
-            # variance_mean.update(variance, inputs.size(0))
-            # batch_mean.update(batch_norm)
-        # return losses.avg, norm_mean.avg, variance_mean.avg, batch_mean.avg
 
     def train_batch(self, imgs, targets, idx, args):
+        self.model.train()
 
-        self.model.zero_grad()
-        self.optimizer.zero_grad()
+        self.model.zero_grad(True)
+        self.optimizer.zero_grad(True)
 
         if not isinstance(imgs, Tensor):
             imgs = [d.to(device) for d in imgs]
@@ -228,8 +225,8 @@ class RunModel(nn.Module):
         # self.gradient_norm.append(max_grad_norm.item())
         # self.gradient_variance.append(var.item())
         # return train_loss, sample_grad_norm.sqrt().mean().item(),  var.item(), max_grad_norm.item()
-        self.model.zero_grad()
-        self.optimizer.zero_grad()
+        self.model.zero_grad(True)
+        self.optimizer.zero_grad(True)
 
     def validate_test(self, val_loader, model, args):
         model.eval()
@@ -252,20 +249,32 @@ class RunModel(nn.Module):
 
     def compute_hessian(self, args):
         train_loader = ParallelDataloader(self.plain_train_dataset, batch_size=self.n_sample//10, shuffle=True)
-        # self.model.eval()
+        self.model.eval()
 
         hessian_traces: list[float] = []
         for (model, loader) in zip(self.model.models, train_loader.loaders): 
-            inputs, targets, _ = next(iter(loader))
-            inputs, targets = inputs.to(device), targets.to(device)
-            hessian_comp = hessian(model, ClippedCrossEntropyLoss(clip=self.loss_clip), data=(inputs, targets), cuda=True)
-            trace = hessian_comp.trace()
-            model.zero_grad()
-            hessian_traces.append(float(np.mean(trace)))
+            empirical_trace = average_hessian_trace(self.loss_clip, model, SelectedDataFieldDataLoader(loader, [0,1]))
+            # inputs, targets, _ = next(iter(loader))
+            # inputs, targets = inputs.to(device), targets.to(device)
+            # hessian_comp_empirical = hessian(model, ClippedCrossEntropyLoss(clip=self.loss_clip), data=(inputs, targets), cuda=True)
+            # empirical_trace = np.mean(hessian_comp_empirical.trace())
+            # hessian_comp_empirical = None; model.zero_grad(True)
+            
+            population_trace = 0
+            # population_trace = average_hessian_trace(self.loss_clip, model, SelectedDataFieldDataLoader(self.test_loader, [0,1]))
+
+           #  test_inputs, test_targets, _ = next(iter(self.test_loader))
+            # test_inputs, test_targets = test_inputs.to(device), test_targets.to(device)
+            # hessian_comp_population = hessian(model, ClippedCrossEntropyLoss(clip=self.loss_clip), data=(test_inputs, test_targets), cuda=True)
+            # population_trace = np.mean(hessian_comp_population.trace())
+            # hessian_comp_population = None; model.zero_grad(True)
+
+            trace =  empirical_trace - population_trace
+            hessian_traces.append(float(trace))
         return hessian_traces 
 
     def compute_bound(self, args):
-        self.model.train()
+        self.model.eval()
         if args.proxy:
             std_fit = self.fit_subGaussian()
             std_proxy = std_fit
@@ -273,24 +282,30 @@ class RunModel(nn.Module):
             device = next(self.model.parameters()).device
             std_proxy = torch.tensor([self.loss_clip / 2], device=device) 
         hessian_traces = self.compute_hessian(args)
-        hessian_term = 1 / 2 * sum(hessian_traces) / len(hessian_traces)
         results = []
         for bound in self.bounds:
-            C = [3/2 * (std_proxy**2 / self.n_sample * h)**(1/3) for h in hessian_traces]
+            hessian_term = 1 / 2 * sum(hessian_traces) / len(hessian_traces)
+            res = OrderedDict()
+            res['name'] = bound.name
+            res['hessian_term_prior'] = float(self.n_iter * hessian_term)
+            print(res['hessian_term_prior'])
+            C = [3/2 * (std_proxy**2 / self.n_sample * h).abs()**(1/3) for h in hessian_traces]
             trajectory_term, punishment, new_hessian_trace, extra_info = bound.forget(C, self.model, self.train_test_loader, self.test_loader)
             if new_hessian_trace is not None:
                 hessian_term = new_hessian_trace / 2
             A = (std_proxy**2 / self.n_sample * trajectory_term).sqrt()
-            B = self.n_iter * hessian_term
+            B = self.n_iter * abs(hessian_term)
             best_sigma = (A / (2 * B))**(1/3)
             bound_value = 3 * ((A/2)**(2/3)) * (B ** (1/3)) + punishment 
             bound.computed_value = bound_value
 
-            res = OrderedDict()
-            res['name'] = bound.name
+            res['C'] = float(torch.tensor(C, device=A.device).mean())
+            res['trajectory_term_vanilla'] = float(A)
+            res['flatness_term_vanilla'] = float(B)
             res['best_sigma'] = float(best_sigma)
             res['trajectory_term'] = float(A / best_sigma)
             res['flatness_term'] = float(best_sigma ** 2 * B)
+
             res['punishment'] = float(punishment)
             for k, v in extra_info.items():
                 res[k] = v
@@ -310,7 +325,7 @@ class RunModel(nn.Module):
             dist, _, _ = model_.forward(torch.ones(1, 1).to(device) * train_x)
             likelihood = dist.log_prob(losses_all)
             loss = (-likelihood).sum()
-            optimizer_.zero_grad()
+            optimizer_.zero_grad(True)
             loss.backward()
             optimizer_.step()
         _, mean_proxy, std_proxy = model_.forward(torch.ones(1, 1).to(device) * train_x)
@@ -340,6 +355,8 @@ def main():
     exp_dir, id = setup_logging(args)
     seed = args.seed
     torch.manual_seed(seed)  # cpu
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
     torch.cuda.manual_seed(seed)
     random.seed(seed)
     np.random.seed(seed)
@@ -352,12 +369,15 @@ def main():
 
     if args.resume is None:
         tr_losses, tr_acces, ts_losses, ts_acces = runmodel.train_model(args)
-        torch.save(runmodel.state_dict(), os.path.join(exp_dir, id + '.pth'))
+        state_dict = runmodel.state_dict()
+        torch.save(state_dict, os.path.join(exp_dir, id + '.pth'))
     else:
         logging.info(f"Loading from {exp_dir}/{id}.pth")
         state_dict = torch.load(os.path.join(exp_dir, id + '.pth'))
         runmodel.load_state_dict(state_dict, strict=True)
         state_dict = None
+        ts_loss, test_acc = runmodel.validate_test(runmodel.test_loader, runmodel.model, args)
+        print(ts_loss, test_acc)
 
     results = runmodel.compute_bound(args)
     for res in results:

code/BoundVerify_DynamicGradientClipping/myhessian.py
@@ -28,7 +28,7 @@ def _inner_product(a: 'list[Tensor]', b: 'list[Tensor]'):
         res = res + torch.inner(aa.flatten(), bb.flatten())
     return res
 
-def iHVP(parallel_model: ParallelModel, hessians:'list[list[Union[Tuple[DataLoader, float], float]]]', b: 'list[list[Tensor]]', epsilon: float, max_iteration=-1, clip=None):
+def iHVP(parallel_model: ParallelModel, hessians:'list[list[Union[Tuple[DataLoader, float], float]]]', b: 'list[list[Tensor]]', epsilon: float, max_iteration=100, clip=None):
     with torch.no_grad():
         vs = [[torch.zeros_like(p) for p in m] for m in b]
         rs = [[p + 0 for p in m] for m in b]

code/BoundVerify_DynamicGradientClipping/parallel.py
@@ -65,7 +65,7 @@ class ParallelModel(nn.Module):
                 mean = p.mean(dim=1, keepdim=True)
 
             delta = delta.reshape(p.shape)
-            res = (p - mean - delta).square().flatten(0, 1).mean(dim=0).sum()
+            res = (p - mean + delta).square().flatten(0, 1).mean(dim=0).sum()
         return res
 
     @torch.no_grad()