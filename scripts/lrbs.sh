4e45134fab3adc4aab48c4008b734935ee8e60cf
pengze <pengze@smail.nju.edu.cn>
2024-11-09 16:36:22 +0000

    optimized ViT experiments

--- scripts/lrbs.sh
+++ scripts/lrbs.sh
@@ -2,8 +2,8 @@ if [[ "$TASK" == "cifar10_vit" ]]; then
     learning_rates=(1e-4 5e-4 1e-3 2e-3 4e-3 8e-3 1e-2 2e-2)
     batch_sizes=(480 240 120 60 30)
 else
-    learning_rates=(5e-4 0.001 0.005 0.01 0.02)
-    batch_sizes=(480 240 120 60 30)
+    learning_rates=(5e-4 0.001 0.005 0.01 0.02 0.1)
+    batch_sizes=(60 480 240 120 30)
 fi
 
 
