fix batch-averaged gradients when estimating empirical gradients for optimization. This makes bound loose when training batch size is large

bounds.py
@@ -149,23 +149,28 @@ class TerminalDispersionBound(Bound):
         return float(term_disp / self.n_iter)
     
     def gradients(self, parallel_model: ParallelModel, data_loader: ParallelDataloader):
-        parallel_loss = ParallelLoss(loss_fn=ClippedCrossEntropyLoss(clip=self.clip)) 
+        parallel_loss = ParallelLoss(loss_fn=ClippedCrossEntropyLoss(clip=self.clip, reduction='sum')) 
         parallel_model.zero_grad(True)
         device = next(parallel_model.parameters()).device
+        count = [0 for _ in parallel_model]
         for data in data_loader:
             data = tuple(data)
             X = data[0]; Y = data[1]
             if isinstance(X, Tensor):
                 X, Y = X.to(device), Y.to(device)
+                lens_y = [len(Y) for _ in range(len(parallel_model))]
             else:
                 X = [x.to(device) for x in X]
                 Y = [y.to(device) for y in Y]
+                lens_y = [len(y) for y in Y]
+            
             output = parallel_model(X)
             loss = parallel_loss(output, Y)
+            count = [c + len_y for c, len_y in zip(count, lens_y)]
             loss.backward()
 
         res = [
-            [p.grad / len(data_loader) if p.grad is not None else torch.zeros_like(p) for p in m.parameters() ] for m in parallel_model.models
+            [p.grad / c if p.grad is not None else torch.zeros_like(p) for p in m.parameters() ] for c, m in zip(count, parallel_model.models)
         ]
 
         parallel_model.zero_grad(True)

main.py
@@ -223,7 +223,7 @@ class RunModel(nn.Module):
                             self.epoch, tr_loss, ts_loss, ts_loss - tr_loss, train_acc, test_acc, 100-test_acc,
                             (time.time() - t))
 
-                if (self.epoch >= 190 and train_acc < 70):
+                if self.epoch >= 190 and (train_acc < 70 or (self.args.arch == 'resnet' and self.args.batch_size >= 240 and train_acc < 95)):
                     logging.info("Hopless. Exiting...")
                     exit(1)
 