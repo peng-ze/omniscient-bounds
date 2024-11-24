62992127a9cc925275eed783e60b357803177b9a
pengze <pengze@smail.nju.edu.cn>
2024-11-24 14:50:59 +0000

    fix bug when initializing unbiased Linears

--- utils.py
+++ utils.py
@@ -224,13 +224,16 @@ def weight_init(m):
             init.normal_(m.bias.data)
     elif isinstance(m, nn.BatchNorm1d):
         init.normal_(m.weight.data, mean=1, std=0.02)
-        init.constant_(m.bias.data, 0)
+        if m.bias is not None:
+            init.constant_(m.bias.data, 0)
     elif isinstance(m, nn.BatchNorm2d):
         init.normal_(m.weight.data, mean=1, std=0.02)
-        init.constant_(m.bias.data, 0)
+        if m.bias is not None:
+            init.constant_(m.bias.data, 0)
     elif isinstance(m, nn.Linear):
         init.xavier_normal_(m.weight.data)
-        init.normal_(m.bias.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
         
 def checkdir(directory):
             if not os.path.exists(directory):
